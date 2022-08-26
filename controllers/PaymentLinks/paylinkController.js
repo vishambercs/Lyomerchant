6e26b9b13a1bdab8872d06a9cb6566d21f3ee8b9
rrkvmm <rasinraj.kuyyanangadan@vaimanagement.co>
2022-08-26 12:05:14 +0400

    paylinkcontroller

--- controllers/PaymentLinks/paylinkController.js
+++ controllers/PaymentLinks/paylinkController.js
@@ -144,43 +144,171 @@ module.exports =
     },
 
 
-    // async createFastCode(req, res) {
-    //     var merchantKey = req.headers.authorization
-    //     let response = ''
-        
-    //     // let flag = await fastPaymentCode.findOne({merchatId : merchantKey}).then(async (val) => {
-    //     // console.log("flag",flag,val)   
-    //     // })         
-    //     console.log(req.body.businessName)
-    //     let businessName = req.body.businessName
-    //     if(!businessName){
+    async assignPaymentLinkMerchantWallet(req, res) {
+        try {
+            var merchantKey   = req.headers.authorization
+            var networkType   = req.body.networkType
+            var callbackURL   = req.body.callbackURL
+            var securityHash  = req.body.securityHash
+            var orderid       = req.body.orderid
+            var security_hash = (merchantKey + networkType + callbackURL + process.env.BASE_WORD_FOR_HASH)
+            var hash          = CryptoJS.MD5(security_hash).toString();
+            let account       = await poolwalletController.getPoolWalletID(networkType) 
+            if (hash == securityHash) 
+            {
+                let currentDateTemp = Date.now();
+                let currentDate = parseInt((currentDateTemp / 1000).toFixed());
+                const newRecord = new paymentLinkTransactionPool({
+                    id: crypto.randomBytes(20).toString('hex'),
+                    api_key: req.headers.authorization,
+                    poolwalletID: account.id,
+                    amount: req.body.amount,
+                    currency: req.body.currency,
+                    callbackURL: req.body.callbackURL,
+                    orderid: req.body.orderid,
+                    clientToken: req.body.token,
+                    status: 0,
+                    walletValidity: currentDate,
+                    timestamps : new Date().getTime()
+                });
+                newRecord.save().then(async (val) => {
+                    await poolWallet.findOneAndUpdate({ 'id': val.poolwalletID }, { $set: { status: 1 } })
+                    let data = { transactionID: val.id, address: account.address, walletValidity: val.walletValidity }
+                    res.json({ status: 200, message: "POS Wallet Assigned Successfully", data: data })
+                }).catch(error => {
+                    res.json({ status: 400, data: {}, message: error })
+                })
+            }
+            else 
+            {
+                res.json({ status: 400, data: {}, message: "Invalid Hash" })
+            }
+        }
+        catch (error) {
+            console.log(error)
+            res.json({ status: 400, data: {}, message: "Unauthorize Access" })
+        }
+    },
+    async getPaymentLinkTransList(req, res) {
+        try {
+          
+           let searchParameters = { "api_key": req.headers.authorization}
+           
+           if( (req.body.networkid  != "" && req.body.networkid  != undefined) && (req.body.status  != "" && req.body.status  != undefined))
+           {
+            searchParameters    = { "api_key": req.headers.authorization, "poolWallet.network_id": req.body.networkid, "status": parseInt(req.body.status) }
+           }   
+           else if(req.body.networkid  != "" && req.body.networkid  != undefined )
+           {
+            searchParameters    = { "api_key": req.headers.authorization, "poolWallet.network_id": req.body.networkid }
+           }   
+           else if(req.body.status  != "" && req.body.status  != undefined )
+           {
+            searchParameters    = { "api_key": req.headers.authorization,"status": parseInt(req.body.status) }
+           }
+            await paymentLinkTransactionPool.aggregate(
+                [
+                    {
+                        $lookup: {
+                            from: "poolwallets", // collection to join
+                            localField: "poolwalletID",//field from the input documents
+                            foreignField: "id",//field from the documents of the "from" collection
+                            as: "poolWallet"// output array field
+                        },
+
+                    }, {
+                        $lookup: {
+                            from: "networks", // collection to join
+                            localField: "poolWallet.network_id",//field from the input documents
+                            foreignField: "id",//field from the documents of the "from" collection
+                            as: "networkDetails"// output array field
+                        }
+                    },
+                    {
+                        $lookup: {
+                            from: "transcationlogs", // collection to join
+                            localField: "id",//field from the input documents
+                            foreignField: "trans_pool_id",//field from the documents of the "from" collection
+                            as: "transactionDetails"// output array field
+                        }
+                    },
+                    {
+                        $lookup: {
+                            from: "merchantstores", // collection to join
+                            localField: "api_key",//field from the input documents
+                            foreignField: "storeapikey",//field from the documents of the "from" collection
+                            as: "storeDetails"// output array field
+                        }
+                    },
+                    {
+                        $lookup: 
+                        {
+                            from: "clients", // collection to join
+                            localField: "storeDetails.clientapikey",//field from the input documents
+                            foreignField: "api_key",//field from the documents of the "from" collection
+                            as: "clientDetails"// output array field
+                        }
+                    },
+                    { $match: serachParameters },
+                    {
+                        "$project": 
+                    {
+                            "poolWallet.privateKey": 0,
+                            "poolWallet.balance": 0,
+                            "poolWallet.id": 0,
+                            "poolWallet._id": 0,
+                            "poolWallet.status": 0,
+                            "poolWallet.__v": 0,
+                            "networkDetails.__v": 0,
+                            "networkDetails.nodeUrl": 0,
+                            "networkDetails.created_by": 0,
+                            "networkDetails.createdAt": 0,
+                            "networkDetails.updatedAt": 0,
+                            "networkDetails.libarayType": 0,
+                            "networkDetails.contractAddress": 0,
+                            "networkDetails.contractABI": 0,
+                            "networkDetails.apiKey": 0,
+                            "networkDetails.transcationurl": 0,
+                            "networkDetails.scanurl": 0,
+                            "networkDetails.status": 0,
+                            "networkDetails.gaspriceurl": 0,
+                            "networkDetails.latest_block_number": 0,
+                            "networkDetails.processingfee": 0,
+                            "networkDetails.transferlimit": 0,
+                            "networkDetails.deleted_by": 0,
+                            "networkDetails.icon": 0,
+                            "clientDetails.token": 0,
+                            "clientDetails.secret": 0,
+                            "clientDetails.qrcode": 0,
+                            "clientDetails.hash": 0,
+                            "clientDetails.emailstatus": 0,
+                            "clientDetails.loginstatus": 0,
+                            "clientDetails.emailtoken": 0,
+                            "clientDetails.status": 0,
+                            "clientDetails.two_fa": 0,
+                            "clientDetails.password": 0,
+                            "clientDetails.kycLink": 0,
+                            "storeDetails.qrcode": 0,
+                            "storeDetails.status": 0,
+                            "storeDetails.created_by": 0,
+                            "networkDetails._id": 0
+                        }
+                    }
+                ]).then(async (data) => {
+                    res.json({ status: 200, message: "Shop Trans List", data: data })
+                }).catch(error => {
+                    console.log("get_clients_data", error)
+                    res.json({ status: 400, data: {}, message: error })
+                })
+                             
             
-    //         let code = parseInt(Math.random() * (1000000 - 100000));
-    //         let fastCodeObject = { "businessName": businessName, "fastCode": code, "status": "active" }
-    //         //console.log("------------",req.body.businessName)
-    
-    //         try {
-    //             let new_record = new fastPaymentCode({
-    //                 id: mongoose.Types.ObjectId(),
-    //                 businessName: req.body.businessName,
-    //                 merchantId: merchantKey,
-    //                 fastCodes: fastCodeObject,
-    //             })
-    //             console.log(new_record)
-    //             response = await new_record.save()
-    //             message = "fast code created"
-    //             status = 200
-    //         }
-    //         catch (error) {
-    
-    //             message = error
-    //             status = 400
-    //         }
-    //     }
-        
-        
-    //     res.json({ status: status, data: response, message: message })
-    // },
+        }
+        catch (error) 
+        {
+            console.log(error)
+            res.json({ status: 400, data: {}, message: "Unauthorize Access" })
+        }
+    },
 
     async verifyFastPayment(req, res) {
         console.log("verify", req.headers.authorization, req.body.businessName)
