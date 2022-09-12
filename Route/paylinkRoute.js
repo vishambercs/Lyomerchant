5df314d05288924da88ff784e46ac13a1a0dffc0
Vishamber Lal <v.c@vaimanagement.co>
2022-09-12 14:42:35 +0400

    Changes

--- Route/paylinkRoute.js
+++ Route/paylinkRoute.js
@@ -23,7 +23,7 @@ const Auth = require('../Validation/Auth');
 router.post('/storeInvoice',        Auth.paylink_have_access,payLinkController.storeInvoice);
 router.post('/paymentLink',         Auth.paylink_have_access,payLinkController.getPaymentLink);
 router.post('/getAllInvoices',       Auth.paylink_have_access,payLinkController.getAllInvoices);
-router.post('/verifyPaymentLink',    Auth.paylink_have_access,payLinkController.verifyPaymentLink);
+router.post('/verifyPaymentLink',    Auth.public_paylink_access,payLinkController.verifyPaymentLink);
 router.post('/createFastCode',       Auth.paylink_have_access,payLinkController.createFastCode);
 router.post('/verifyFastPayment',   Auth.paylink_have_access, payLinkController.verifyFastPayment);
 router.post('/verifyFastCode',       Auth.paylink_have_access,payLinkController.verifyFastCode);
