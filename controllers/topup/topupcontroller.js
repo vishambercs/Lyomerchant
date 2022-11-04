27629a32a7ef3e2a764d16d33a80beda23772c8f
Vishamber Lal <v.c@vaimanagement.co>
2022-11-04 10:30:58 +0400

    Changes

--- controllers/topup/topupcontroller.js
+++ controllers/topup/topupcontroller.js
@@ -1,108 +1,104 @@
-const topup = require('../../Models/topup');
-const poolWallet = require('../../Models/poolWallet');
-const networks = require('../../Models/network');
+
+const Utility = require('../../common/Utility');
 var otpGenerator = require('otp-generator')
 var mongoose = require('mongoose');
-var poolwalletController = require('../poolwalletController');
+require('dotenv').config()
+
 module.exports =
 {
     
-    async create_top_payment(req, res) {
+    async pluginallNetworks(req, res) {
         try {
             var merchantKey = req.headers.authorization
-            var networkType = req.body.networkType
-            var orderid = req.body.orderid
-            let currentDateTemp = Date.now();
-            let currentDate = parseInt((currentDateTemp / 1000).toFixed());
-            let account = await poolwalletController.getPoolWalletID(networkType)
-
-            const transactionPool = new topup({
-                id: mongoose.Types.ObjectId(),
-                api_key: req.headers.authorization,
-                poolwalletID: account.id,
-                amount: 0,
-                currency: req.body.currency,
-                callbackURL: req.body.callbackurl,
-                apiredirectURL: req.body.apiredirecturl,
-                errorurl: req.body.errorurl,
-                orderid: req.body.orderid,
-                status: 0,
-                walletValidity: currentDate,
-                remarks: req.body.remarks,
-                timestamps: new Date().getTime()
-            });
-            transactionPool.save().then(async (val) => {
-                await poolWallet.findOneAndUpdate({ 'id': val.poolwalletID }, { $set: { status: 1 } })
-                let url = process.env.TOP_UP_URL + val.id
-                let data = { url: url }
-                res.json({ status: 200, message: "Assigned Merchant Wallet Successfully", data: data })
-            }).catch(error => {
-                console.log("error", error)
-                res.json({ status: 400, data: {}, message: "Please Contact Admin" })
-            })
-        }
+            const url      =  "http://10.101.12.136:5003"+"/v1/pluginallNetworks"
+            let headers  =  {}
+            let response =  await Utility.Post_Request_By_Axios(url,headers,merchantKey)
+            console.log(response)
+            var stringify_response  = JSON.parse(response)
+          
+            res.json(stringify_response)
+         }
         catch (error) {
             console.log(error)
             res.json({ status: 400, data: {}, message: "Error" })
         }
     },
-    async get_top_payment_data(req, res) {
+ 
+    async assigntopupMerchantWallet(req, res) {
         try {
-            let transactionPool = await topup.findOne({ id: req.body.id , status : 0 })
-            console.log("transactionPool",transactionPool)
-            if (transactionPool == null) {
-                return res.json({ status: 400, message: "Invalid Trans ID", data: {} })
+            var merchantKey         =  req.headers.authorization
+            const url               =  process.env.API_URL+"/v1/assigntopupMerchantWallet"
+            let parameters          =  {
+                "networkType"       : req.body.networkType,
+                "orderid"           : req.body.orderid,
+                "currency"          : req.body.currency,
+                "callbackurl"       : req.body.callbackurl,
+                "apiredirecturl"    : req.body.apiredirecturl,
+                "errorurl"          : req.body.errorurl,
+                "amount"            : req.body.amount,
             }
-            let transWallet = await poolWallet.findOne({ id: transactionPool.poolwalletID })
-            console.log("transWallet",transWallet)
-            if (transWallet == null) {
-                return res.json({ status: 400, message: "Invalid Trans ID", data: {} })
-            }
-            let network = await networks.findOne({ id: transWallet.network_id })
-            let data =
-            {
-                transactionID   : transactionPool.id,
-                address         : transWallet.address,
-                walletValidity  : transactionPool.walletValidity,
-                amount          : transactionPool.amount,
-                key             : transactionPool.api_key,
-                apiredirecturl  : transactionPool.apiredirectURL,
-                errorurl        : transactionPool.errorurl,
-                orderid         : transactionPool.orderid,
-                network         : network.network,
-                coin            : network.coin
-            }
-            res.json({ status: 200, message: "Get The Data", data: data })
-
-
-        }
+            let response            =  await Utility.Post_Request_By_Axios(url,parameters,merchantKey)
+            var stringify_response  = JSON.parse(response)
+            res.json(stringify_response)
+         }
         catch (error) {
             console.log(error)
-            res.json({ status: 400, data: {}, message: "Unauthorize Access" })
+            res.json({ status: 400, data: {}, message: "Error" })
         }
     },
-    async cancelpaymentLink(req, res) {
+  
+    async getTranscationDataofTopup(req, res) {
         try {
-            let tranPool     = await topup.findOne({id  : req.body.id })
-            if(tranPool == null)
+            var merchantKey         =  req.headers.authorization
+            const url               =  process.env.API_URL+"/v1/getTranscationDataofTopup"
+            let parameters          =  
             {
-               return  res.json({ status: 400, message: "Invalid Trans ID", data: {} })
+                "id"       : req.body.id,
             }
-            let transactionPool = await topup.findOneAndUpdate({ 'id':tranPool.id  }, { $set: { "status" : 5 , "remarks" : "By Client Canceled" , "canceled_at" : new Date().toString() }} ,{ returnDocument: 'after' })
-            let data = 
-            { 
-                transactionID: transactionPool.id, 
-                orderid     : transactionPool.orderid,
-            }
-            res.json({ status: 200, message: "Get The Data", data: data })
-
-            
-        }
+            let response            =  await Utility.Post_Request_By_Axios(url,parameters,merchantKey)
+            var stringify_response  = JSON.parse(response)
+            res.json(stringify_response)
+         }
         catch (error) {
             console.log(error)
-            res.json({ status: 400, data: {}, message: "Unauthorize Access" })
+            res.json({ status: 400, data: {}, message: "Error" })
         }
-    },   
+    },
+
+    async getTransStatus(req, res) {
+        try {
+            var merchantKey         =  req.headers.authorization
+            const url               =  process.env.API_URL+"/v1/getTransStatus"
+            let parameters          =  
+            {
+                "transid"       : req.body.id,
+            }
+            let response            =  await Utility.Post_Request_By_Axios(url,parameters,merchantKey)
+            var stringify_response  = JSON.parse(response)
+            res.json(stringify_response)
+         }
+        catch (error) {
+            console.log(error)
+            res.json({ status: 400, data: {}, message: "Error" })
+        }
+    },
+    async canceltopup(req, res) {
+        try {
+            var merchantKey         =  req.headers.authorization
+            const url               =  process.env.API_URL+"/v1/canceltopup"
+            let parameters          =  
+            {
+                "id"       : req.body.id,
+            }
+            let response            =  await Utility.Post_Request_By_Axios(url,parameters,merchantKey)
+            var stringify_response  = JSON.parse(response)
+            res.json(stringify_response)
+         }
+        catch (error) {
+            console.log(error)
+            res.json({ status: 400, data: {}, message: "Error" })
+        }
+    },
 }
 
 
